43fdcacee56d2cfb383877804006467be8e30590
Daniel Resino <daniel.resinojimenez@riberadeltajo.es>
2025-02-20 15:49:01 +0100

    Actualizaciones en el index

--- front/index.php
+++ front/index.php
@@ -1,25 +1,44 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
-    <title>Document</title>
+    <title>RiberaPets</title>
 </head>
+
 <body class="bg-blue-100 min-h-screen">
     <header class="bg-blue-200 flex justify-between items-center p-8">
-        <a href="./index.php"><h1 class="text-purple-500 font-bold text-5xl">Biblioteca Ribera</h1></a>
+        <a href="./index.php">
+            <h1 class="text-purple-500 font-bold text-5xl">Ribera Pets</h1>
+        </a>
         <?php
-            session_start();
-            if(isset($_SESSION['token'])){
+        session_start();
+        if (isset($_SESSION['token'])) {
         ?>
-        <div class="flex justify-between gap-8">
-            <a class="text-purple-500 font-semibold text-xl" href="index.php"><p>Libros</p></a>
-            <a class="text-purple-500 font-semibold text-xl" href="index.php?controller=PrestamosController&action=mostrarPrestamos"><p>Mis libros prestados</p></a>
-            <a class="text-purple-500 font-semibold text-xl" href="index.php?controller=UsersController&action=cerrarSesion"><p>Log Out</p></a>
-        </div>
+            <div class="flex justify-between gap-8">
+                <a class="text-purple-500 font-semibold text-xl" href="index.php">
+                    <p>Home</p>
+                </a>
+                <a class="text-purple-500 font-semibold text-xl" href="index.php?controller=&action=">
+                    <p>Empleados</p>
+                </a>
+                <a class="text-purple-500 font-semibold text-xl" href="index.php?controller=&action=">
+                    <p>Clientes</p>
+                </a>
+                <a class="text-purple-500 font-semibold text-xl" href="index.php?controller=&action=">
+                    <p>Servicios</p>
+                </a>
+                <a class="text-purple-500 font-semibold text-xl" href="index.php?controller=&action=">
+                    <p>Perros</p>
+                </a>
+                <a class="text-purple-500 font-semibold text-xl" href="index.php?controller=&action=">
+                    <p>Log Out</p>
+                </a>
+            </div>
         <?php
-            }
+        }
         ?>
     </header>
     <div class="container mx-auto text-center">
@@ -30,5 +49,5 @@
     </div>
 
 </body>
-</html>
 
+</html>
