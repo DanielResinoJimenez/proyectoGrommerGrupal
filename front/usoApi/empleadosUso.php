c0db6e8e93f1fdf77312f000fc885d21df5b1ebc
Daniel Resino <daniel.resinojimenez@riberadeltajo.es>
2025-02-26 20:58:26 +0100

    dark mode en serviciosView

--- front/usoApi/empleadosUso.php
+++ front/usoApi/empleadosUso.php
@@ -89,79 +89,50 @@ class EmpleadosUso
         $this->view->showAddEmpleadoForm();
     }
 
-        //funcion para borrar un empleado
-        public function deleteEmpleado()
-        {
-            // Verificar si se ha confirmado la eliminación
-            if (isset($_POST['confirmar'])) {
-                if ($_POST['confirmar'] == 'sí') {
-                    // URL base de la API local
-                    $base_url = 'http://localhost/gromer/api/controllers/empleadosController.php';
-   
-                    $_POST['accion'] = 'borrar';
-                    $post_url = $base_url;
-                    $ch = curl_init($post_url);
-                    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                    curl_setopt($ch, CURLOPT_POST, true);
-                    curl_setopt($ch, CURLOPT_POSTFIELDS, $_POST);
-                    $post_response = curl_exec($ch);
-  
-                    if ($post_response === false) {
-                        echo 'Error en la petición POST: ' . curl_error($ch);
-                    } else {
-                        $data = json_decode($post_response, true);
-                        $empleadosLista = $data;
-                    }
-                    curl_close($ch);
-                    if (isset($empleadosLista['mensaje']) && $empleadosLista['mensaje'] == 'El empleado no existe') {
-                        echo "<script>alert('" . $empleadosLista['mensaje'] . "');</script>";
-                        $this->showFormController($empleadosLista);
-                        return;
-                    } else {
-                        echo "<script>alert('empleado DNI: " . $_POST['dni'] . " borrado correctamente');</script>";
-                    }
-                    $this->showempleados();
-                } else {
-                    // Si el usuario dice "No", regresar a la vista de empleados
-                    $this->showempleados();
-                }
-            } else {
-                // Mostrar formulario de confirmación
-                echo "<form method='POST' action='http://localhost/gromer/front/index.php?controller=empleadosUso&action=deleteEmpleado'>";
-                echo "<input type='hidden' name='dni' value='" . $_GET['dni'] . "'>";
-                echo "<p class='p-10'>¿Está seguro de eliminar el empleado?</p>";
-                echo "<button type='submit' class='bg-green-500 text-white mx-2 px-4 py-2 rounded' name='confirmar' value='sí'>Sí</button>";
-                echo "<button type='submit' class='bg-red-500 text-white mx-2 px-4 py-2 rounded' name='confirmar' value='no'>No</button>";
-                echo "</form>";
-            }
-        }
+    //funcion para borrar un empleado
+    public function deleteEmpleado()
+    {
+        // Verificar si se ha confirmado la eliminación
+        if (isset($_POST['confirmar'])) {
+            if ($_POST['confirmar'] == 'sí') {
+                // URL base de la API local
+                $base_url = 'http://localhost/gromer/api/controllers/empleadosController.php';
 
-        public function createEmpleado()
-        {
-            // URL base de la API local
-            $base_url = 'http://localhost/gromer/api/controllers/empleadosController.php';
-     
-            // Petición POST
-            $_POST['accion'] = 'crear';
-            $post_url = $base_url;
-            $ch = curl_init($post_url);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch, CURLOPT_POST, true);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, $_POST);
-            $post_response = curl_exec($ch);
-            if ($post_response === false) {
-                echo 'Error en la petición POST: ' . curl_error($ch);
+                $_POST['accion'] = 'borrar';
+                $post_url = $base_url;
+                $ch = curl_init($post_url);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                curl_setopt($ch, CURLOPT_POST, true);
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $_POST);
+                $post_response = curl_exec($ch);
+
+                if ($post_response === false) {
+                    echo 'Error en la petición POST: ' . curl_error($ch);
+                } else {
+                    $data = json_decode($post_response, true);
+                    $empleadosLista = $data;
+                }
+                curl_close($ch);
+                if (isset($empleadosLista['mensaje']) && $empleadosLista['mensaje'] == 'El empleado no existe') {
+                    echo "<script>alert('" . $empleadosLista['mensaje'] . "');</script>";
+                    $this->showFormController($empleadosLista);
+                    return;
+                } else {
+                    echo "<script>alert('empleado DNI: " . $_POST['dni'] . " borrado correctamente');</script>";
+                }
+                $this->showempleados();
             } else {
                 // Si el usuario dice "No", regresar a la vista de empleados
                 $this->showempleados();
             }
-            curl_close($ch);
-             if (isset($empleadosLista['mensaje']) && $empleadosLista['mensaje'] == 'El empleado ya está dado de alta') {
-                 echo "<script>alert('" . $empleadosLista['mensaje'] . "');</script>";
-                 $this->showFormController($empleadosLista);            
-                 return;
-             }
-             $this->showempleados();
+        } else {
+            // Mostrar formulario de confirmación
+            echo "<form method='POST' action='http://localhost/gromer/front/index.php?controller=empleadosUso&action=deleteEmpleado'>";
+            echo "<input type='hidden' name='dni' value='" . $_GET['dni'] . "'>";
+            echo "<p class='p-10'>¿Está seguro de eliminar el empleado?</p>";
+            echo "<button type='submit' class='bg-green-500 text-white mx-2 px-4 py-2 rounded' name='confirmar' value='sí'>Sí</button>";
+            echo "<button type='submit' class='bg-red-500 text-white mx-2 px-4 py-2 rounded' name='confirmar' value='no'>No</button>";
+            echo "</form>";
         }
     }
 
--- front/views/serviciosView.php
+++ front/views/serviciosView.php
@@ -8,32 +8,32 @@ class ServiciosView
     public function showServices($listaServices)
     {
 ?>
-        <div class="bg-white p-4 rounded shadow mb-4 overflow-x-auto">
-            <h2 class="text-xl font-bold mb-2">Lista de Servicios</h2>
-            <a href="http://localhost/gromer/front/index.php?controller=serviciosUso&action=showForm"><button>Crear Servicio</button></a>
-            <table class="min-w-full divide-y divide-gray-200 text-sm">
-                <thead class="bg-gray-50">
+        <div class="bg-white p-4 rounded shadow mb-4 overflow-x-auto dark:bg-gray-800">
+            <h2 class="text-xl font-bold mb-2 dark:text-purple-400">Lista de Servicios</h2>
+            <a href="http://localhost/gromer/front/index.php?controller=serviciosUso&action=showForm"><button class="bg-green-500 text-white px-4 py-2 rounded m-4 dark:bg-green-700">Crear Servicio</button></a>
+            <table class="min-w-full divide-y divide-gray-200 text-sm dark:divide-gray-700">
+                <thead class="bg-gray-50 dark:bg-gray-700">
                     <tr>
-                        <th class="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Codigo </th>
-                        <th class="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Nombre</th>
-                        <th class="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Precio</th>
-                        <th class="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Descripcion</th>
+                        <th class="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider dark:text-gray-300">Codigo </th>
+                        <th class="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider dark:text-gray-300">Nombre</th>
+                        <th class="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider dark:text-gray-300">Precio</th>
+                        <th class="px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider dark:text-gray-300">Descripcion</th>
                         <th></th>
                     </tr>
                 </thead>
-                <tbody id="serviciosLista" class="bg-white divide-y divide-gray-200">
+                <tbody id="serviciosLista" class="bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700">
                     <?php
 
                     if (is_array($listaServices)) {
                         foreach ($listaServices as $servicio) {
                             echo "<tr>";
-                            echo "<td class='px-4 py-2 whitespace-nowrap'>{$servicio['Codigo']}</td>";
-                            echo "<td class='px-4 py-2 whitespace-nowrap'>{$servicio['Nombre']}</td>";
-                            echo "<td class='px-4 py-2 whitespace-nowrap'>{$servicio['Precio']}</td>";
-                            echo "<td class='px-4 py-2 whitespace-nowrap'>{$servicio['Descripcion']}</td>";
+                            echo "<td class='px-4 py-2 whitespace-nowrap dark:text-gray-300'>{$servicio['Codigo']}</td>";
+                            echo "<td class='px-4 py-2 whitespace-nowrap dark:text-gray-300'>{$servicio['Nombre']}</td>";
+                            echo "<td class='px-4 py-2 whitespace-nowrap dark:text-gray-300'>{$servicio['Precio']}</td>";
+                            echo "<td class='px-4 py-2 whitespace-nowrap dark:text-gray-300'>{$servicio['Descripcion']}</td>";
                             // echo "<form method='POST' action='http://localhost/Proyecto_DWES/api/controllers/clientesController.php?accion=borrar' style='display:inline;'>";
                             // echo "<input type='hidden' name='dni' value='{$cliente['Dni']}'>";
-                            echo "<td class='px-4 py-2 whitespace-nowrap'><a href='http://localhost/gromer/front/index.php?controller=serviciosUso&action=showEditForm&id=" . $servicio['Codigo'] . "&precio=" . $servicio['Precio'] . "' ><button class='bg-red-500 text-white px-4 py-2 rounded'>Editar</button></a></td>";
+                            echo "<td class='px-4 py-2 whitespace-nowrap dark:text-gray-300'><a href='http://localhost/gromer/front/index.php?controller=serviciosUso&action=showEditForm&id=" . $servicio['Codigo'] . "&precio=" . $servicio['Precio'] . "' ><button class='bg-red-500 text-white px-4 py-2 rounded'>Editar</button></a></td>";
                             // echo "</form>";
                             echo "</tr>";
                         }
@@ -55,8 +55,8 @@ class ServiciosView
                 <form id="editarServicioForm" class="space-y-4 text-left" method="POST" action="http://localhost/gromer/front/index.php?controller=serviciosUso&action=editService">
                     <div>
                         <input type="text" id="id" name="id" class="hidden" value=<?php echo $_GET['id'] ?>>
-                        <label for="precio" class="block text-sm font-medium text-gray-700">Precio</label>
-                        <input type="number" step=0.01 id="precio" name="precio" value=<?php echo $_GET['precio'] ?> class="mt-1 block w-full border border-gray-300 dark:border-gray-700 rounded-md shadow-sm p-2 bg-white text-black">
+                        <label for="precio" class="block text-sm font-medium text-gray-700 dark:text-gray-300">Precio</label>
+                        <input type="number" step=0.01 id="precio" name="precio" value=<?php echo $_GET['precio'] ?> class="mt-1 block w-full border border-gray-300 dark:border-gray-700 rounded-md dark:bg-gray-700 dark:border-gray-600 dark:text-white shadow-sm p-2 bg-white text-black">
                     </div>
                     <div class="flex justify-end">
                         <a href='http://localhost/gromer/front/index.php?controller=serviciosUso&action=showServicios'><button type="button" class="bg-gray-200 text-black px-4 py-2 rounded mr-2">Cancelar</button></a>
@@ -70,32 +70,32 @@ class ServiciosView
         {
             ?>
                 <!-- Modal para crear servicios -->
-                <div id="modal" class="fixed inset-0 bg-gray-800 bg-opacity-75 flex items-center justify-center">
-                    <div class="bg-white p-6 rounded shadow-lg w-1/3">
-                        <h2 class="text-2xl font-bold mb-4 text-black ">Crear Servicio</h2>
+                <div id="modal" class="fixed inset-0 bg-gray-800 bg-opacity-75 flex items-center justify-center dark:bg-gray-900 dark:bg-opacity-80">
+                    <div class="bg-white p-6 rounded shadow-lg w-1/3 dark:bg-gray-800">
+                        <h2 class="text-2xl font-bold mb-4 text-black dark:text-white">Crear Servicio</h2>
                         <form id="crearServicioForm" class="space-y-4 text-left" method="POST" action="http://localhost/gromer/front/index.php?controller=serviciosUso&action=createService">
                             <div>
-                                <label for="belleza" class="block text-sm font-medium text-gray-700 ">Código</label>
-                                <select id="belleza" name="belleza" class="mt-1 block w-full border border-gray-300 dark:border-gray-700 rounded-md shadow-sm p-2 bg-white text-black ">
+                                <label for="belleza" class="block text-sm font-medium text-gray-700 dark:text-gray-300 ">Código</label>
+                                <select id="belleza" name="belleza" class="mt-1 block w-full border border-gray-300 dark:border-gray-700 rounded-md dark:bg-gray-700 dark:border-gray-600 dark:text-white shadow-sm p-2 bg-white text-black ">
                                     <option value="true">Belleza</option>
                                     <option value="false">Nutrición</option>
                                 </select>
                             </div>
                             <div>
-                                <label for="nombre" class="block text-sm font-medium text-gray-700 ">Nombre</label>
-                                <input type="text" id="nombre" name="nombre" class="mt-1 block w-full border border-gray-300 dark:border-gray-700 rounded-md shadow-sm p-2 bg-white text-black" required>
+                                <label for="nombre" class="block text-sm font-medium text-gray-700 dark:text-gray-300 ">Nombre</label>
+                                <input type="text" id="nombre" name="nombre" class="mt-1 block w-full border border-gray-300 dark:border-gray-700 rounded-md dark:bg-gray-700 dark:border-gray-600 dark:text-white shadow-sm p-2 bg-white text-black" required>
                             </div>
                             <div>
-                                <label for="precio" class="block text-sm font-medium text-gray-700 ">Precio</label>
-                                <input type="number" step=0.01 id="precio" name="precio" class="mt-1 block w-full border border-gray-300 dark:border-gray-700 rounded-md shadow-sm p-2 bg-white text-black" required>
+                                <label for="precio" class="block text-sm font-medium text-gray-700 dark:text-gray-300 ">Precio</label>
+                                <input type="number" step=0.01 id="precio" name="precio" class="mt-1 block w-full border border-gray-300 dark:border-gray-700 rounded-md dark:bg-gray-700 dark:border-gray-600 dark:text-white shadow-sm p-2 bg-white text-black" required>
                             </div>
                             <div>
-                                <label for="descripcion" class="block text-sm font-medium text-gray-700 ">Descripción</label>
-                                <input type="text" id="descripcion" name="descripcion" class="mt-1 block w-full border border-gray-300 dark:border-gray-700 rounded-md shadow-sm p-2 bg-white text-black" required>
+                                <label for="descripcion" class="block text-sm font-medium text-gray-700 dark:text-gray-300 ">Descripción</label>
+                                <input type="text" id="descripcion" name="descripcion" class="mt-1 block w-full border border-gray-300 dark:border-gray-700 rounded-md dark:bg-gray-700 dark:border-gray-600 dark:text-white shadow-sm p-2 bg-white text-black" required>
                             </div>
                             <div class="flex justify-end">
-                                <a href='http://localhost/gromer/front/index.php?controller=serviciosUso&action=showServicios'><button type="button" class="bg-gray-200 text-black  px-4 py-2 rounded mr-2">Cancelar</button></a>
-                                <button type="submit" class="bg-blue-500 dark:bg-blue-600 text-white px-4 py-2 rounded">Crear Servicio</button>
+                                <a href='http://localhost/gromer/front/index.php?controller=serviciosUso&action=showServicios'><button type="button" class="bg-gray-200 text-black  px-4 py-2 rounded mr-2 dark:bg-gray-700">Cancelar</button></a>
+                                <button type="submit" class="bg-blue-500 dark:bg-blue-700 text-white px-4 py-2 rounded">Crear Servicio</button>
                             </div>
                         </form>
                     </div>
